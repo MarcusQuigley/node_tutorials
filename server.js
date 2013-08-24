6967f09b69be58d815d8b358ad62971724481c8c
MarcusQuigley <marcusquigley@gmail.com>
2013-08-24 19:18:08 +0000

    c

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+//var express = require('express');
+//var app = express();
+//app.use(express.logger());
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World!');
+//});
 
-//var http = require('http');
-//console.log("Starting");
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+// console.log("Listening on " + port);
+//});
+
+var http = require('http');
+console.log("Starting");
 //var host = "127.0.0.1";
 //var port = 1337;
 //var server = http.createServer(function(request, response){
