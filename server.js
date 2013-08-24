d99cfe86795f197ba1da2740e9cc819d0fac9058
MarcusQuigley <marcusquigley@gmail.com>
2013-08-24 20:06:19 +0000

    c

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var port =  process.env.PORT || 5000;
 var server = http.createServer(function(request, response){
 	console.log("Received request " + request.url);
 	response.writeHead(200, {"Content-type":"text/plain"});
-	response.end("Hello world at " + dateNow.getDate());
+	response.end("Hello world at " + dateNow);
 });
 server.listen(port, function(){
 	console.log("Listening at :" + port);
