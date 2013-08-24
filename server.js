0ffac3c039f03ea67ded993052fac40dae53bcb7
MarcusQuigley <marcusquigley@gmail.com>
2013-08-24 20:34:26 +0000

    adding files to be read

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 
-//var express = require('express');
-//var app = express();
-//app.use(express.logger());
-
-//app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//});
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-// console.log("Listening on " + port);
-//});
-
 var http = require('http');
 console.log("Starting");
 var dateNow = new Date();
 //var host = "127.0.0.1";
+var fs = require('fs');
 var port =  process.env.PORT || 5000;
 var server = http.createServer(function(request, response){
 	console.log("Received request " + request.url);
-	response.writeHead(200, {"Content-type":"text/plain"});
-	response.end("Hello world at " + dateNow);
+	fs.readFile("./public" + request.url, function(error, data){
+		if (error) {
+			response.writeHead(404, {"Content-type":"text/plain"});
+			response.end("Sorry page can't be found");
+		} else {
+			response.writeHead(200, {"Content-type":"text/plain"});
+                        response.end(data);
+
+		}
+	})
 });
 server.listen(port, function(){
 	console.log("Listening at :" + port);
