c4e1cafbea9ff8489819858c4370c7a63a8b9507
MarcusQuigley <marcusquigley@gmail.com>
2013-08-24 18:47:05 +0000

    switching to tutorial code

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+//var express = require('express');
+//var app = express();
+//app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+//app.get('/', function(request, response) {
+ // response.send('Hello World!');
+//});
+
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+ // console.log("Listening on " + port);
+//});
+
+var http = require('http');
+console.log("Starting");
+var host = "127.0.0.1";
+var port = 1337;
+var server = http.createServer(function(request, response){
+	console.log("Received request " + request.url);
+	response.writeHead(200, {"Content-type":"text/plain"});
+	response.end("Hello world2");
 });
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+server.listen(port, host, function(){
+	console.log("Listening at " + host + ":" + port);
 });
