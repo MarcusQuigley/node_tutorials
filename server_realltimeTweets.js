e654021bf06f39443ccde206d0a62c27c9dd2220
MarcusQuigley <marcusquigley@gmail.com>
2013-08-28 16:50:54 +0000

    adding db open func

--- server_realltimeTweets.js
+++ server_realltimeTweets.js
@@ -1,9 +1,28 @@
 
 var express = require('express');
 var fs = require('fs');
+var mongo = require('mongodb');
 var config = JSON.parse(fs.readFileSync('config.json'));
 var host = config.host;
 var port = process.env.PORT || config.port;
+var db = new mongo.Db("Nodejs_introduction", new mongo.Server(host, port, {}));
+var tweetsCollection;
+
+db.open(function(error){
+	if (error) {
+		console.log("Error opening database:", error);
+	} else {
+		console.log("db open at " + host + ":" + port);
+		db.collection("tweet", function(error, collection){
+			if (error) {		       
+				console.log("Error getting data");
+			} else {
+				tweetsCollection = collection;
+			}
+		});
+	}
+});
+
 
 var app = express();
 
