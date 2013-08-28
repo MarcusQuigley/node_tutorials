d6284cd6f420b2bc1fa1004b67957a23bb2b837e
MarcusQuigley <marcusquigley@gmail.com>
2013-08-28 17:56:39 +0000

    adding tweets to html

--- server_realltimeTweets.js
+++ server_realltimeTweets.js
@@ -4,8 +4,32 @@ var fs = require('fs');
 var mongo = require('mongodb');
 var config = JSON.parse(fs.readFileSync('config.json'));
 var host = config.host;
-var port = process.env.PORT || config.port;
-var db = new mongo.Db("Nodejs_introduction", new mongo.Server(host, port, {}));
+var port = process.env.PORT || 5000;
+
+var app = express();
+
+app.get("/", function(request, response){
+        var content = fs.readFileSync("template.html");
+
+	getTweets(function(tweets){
+		var ul='';
+		tweets.ForEach(function(tweet){
+			ul += "<li><strong>" + tweet.user.screen_name + ": </strong>" + tweet.text + "</li>";
+		});
+		content.toString("utf8").replace("{{INITIAL_TWEETS}}", ul);
+		response.setHeader("Content-Type", "text/html");
+	        response.send(content);
+	});
+});
+
+app.listen(port);
+console.log("listening on port:", port);
+
+
+
+//process.exit(1);
+
+var db = new mongo.Db("Nodejs_introduction", new mongo.Server(host, mongo.Connection.DEFAULT_PORT, {}));
 var tweetsCollection;
 
 db.open(function(error){
@@ -23,15 +47,16 @@ db.open(function(error){
 	}
 });
 
+function getTweets(callback){
+	tweetsCollection.find({}, {"limit":10, "_id":-1}, function(error, cursor){
+		if (error){
+			console.log("Error getting tweets collection", error);
+		} else {
+			cursor.toArray(function(error, tweets){
+				callback(tweets);
+			});
+		}
+	});
 
-var app = express();
+}
 
-app.get("/", function(request, response){
-	var content = fs.readFileSync("template.html");
-	response.setHeader("Content-Type", "text/html");
-	response.send(content);	
-	
-});
-
-app.listen(port);
-console.log("listening on port:", port);
