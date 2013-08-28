9258cba7cf251d0666306dcebabe47f567f43491
MarcusQuigley <marcusquigley@gmail.com>
2013-08-28 14:56:54 +0000

    initial code for realtime tweets

---
+++ server_realltimeTweets.js
@@ -0,0 +1,16 @@
+
+var express = require('express');
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('config.json'));
+var host = config.host;
+var port = process.env.PORT || config.port;
+
+var app = express();
+
+app.get("/", function(request, response){
+	response.send("hello");	
+	
+});
+
+app.listen(port);
+console.log("listening on port:", port);
